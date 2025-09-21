916a456aec8478724da1943456e67f21770c7d4e
anonymous-lad2 <anonymouslad540@gmail.com>
2025-09-21 19:41:54 +0530

    kafka integration

--- order-service/src/main/java/com/happysat/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/happysat/microservices/order/service/OrderService.java
@@ -4,36 +4,48 @@ import com.happysat.microservices.order.client.InventoryClient;
 import com.happysat.microservices.order.dto.OrderRequest;
 import com.happysat.microservices.order.modal.Order;
 import com.happysat.microservices.order.repository.OrderRepository;
+import com.happysat.microservices.order.event.OrderPlacedEvent;
+import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class OrderService {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
 
-        boolean inStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
-        if (inStock) {
-            var order = mapToOrder(orderRequest);
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
+            var orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails()
+                    .email(),
+                    orderRequest.userDetails()
+                            .firstName(),
+                    orderRequest.userDetails()
+                            .lastName());
+            log.info("Start- Sending OrderPlacedEvent {} to Kafka Topic", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End- Sending OrderPlacedEvent {} to Kafka Topic", orderPlacedEvent);
         } else {
-            throw new RuntimeException("Product with Skucode " + orderRequest.skuCode() + "is not in stock");
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
         }
-    }
-
-    private static Order mapToOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.quantity());
-        order.setSkuCode(orderRequest.skuCode());
-        return order;
     }
 }
