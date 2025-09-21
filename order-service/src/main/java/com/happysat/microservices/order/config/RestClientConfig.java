54a093d89252826b206ff42b3c8b28e7741ac726
anonymous-lad2 <anonymouslad540@gmail.com>
2025-09-21 23:44:56 +0530

    observability and tracing using grafana stack

--- order-service/src/main/java/com/happysat/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/happysat/microservices/order/config/RestClientConfig.java
@@ -1,6 +1,8 @@
 package com.happysat.microservices.order.config;
 
 import com.happysat.microservices.order.client.InventoryClient;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.ClientHttpRequestFactories;
 import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
@@ -15,16 +17,19 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import java.time.Duration;
 
 @Configuration
+@RequiredArgsConstructor
 public class RestClientConfig {
 
-    @Value("${inventory.url}")
+    @Value("${inventory.service.url}")
     private String inventoryServiceUrl;
+    private final ObservationRegistry observationRegistry;
 
     @Bean
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
                 .requestFactory(getClientRequestFactory())
+                .observationRegistry(observationRegistry)
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
